61ed513a3fb8dd27fe41e5d28af1010c11740cec
ChihYang218 <chihyang218@gmail.com>
2023-10-19 11:50:35 +0800

    創建主要頁面，並擁有路由
    創建 bmi 與 math運算 的頁面，且都包含 navbar

---
+++ routes/math.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+// 取得 server 端 IP，需要 underscore 模組（開發使用）
+const sip = require('underscore').chain(require('os').networkInterfaces()).values()
+    .flatten().find({family: 'IPv4', internal: false}).value().address;
+console.log('Server IP='+sip);
+
+/* GET home page. */
+router.get('/math', function(req, res, next) {
+  res.render('index', { title: 'Math運算' });
+});
+
+module.exports = router;
