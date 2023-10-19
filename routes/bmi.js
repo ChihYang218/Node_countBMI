5e40d09711ad192700efba401eb6b790e770cda5
ChihYang218 <chihyang218@gmail.com>
2023-10-19 14:44:40 +0800

    解決 index 的格式傳送到其他 router
    問題是出在 res.render 第一個參數，那裡要改成目標檔案

--- routes/bmi.js
+++ routes/bmi.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BMI服務' });
+  res.render('bmi', { title: 'BMI服務' });
 });
 
 module.exports = router;
