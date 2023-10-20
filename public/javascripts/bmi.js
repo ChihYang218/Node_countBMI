9cebae9d57b5bda4e6974a894f8cc93f6bfdde9f
ChihYang218 <chihyang218@gmail.com>
2023-10-20 11:00:18 +0800

    新增身高體重的輸入判斷
    時常遇見不能動的問題是在讀取 id 的時候時常沒有加上'#' 該鞭了QQ

--- public/javascripts/bmi.js
+++ public/javascripts/bmi.js
@@ -1,9 +1,17 @@
 $(function(){
   $('#btnBMI').click( () => {
     let gender = $('input[name="inlineRadioOptions"]:checked').val();
+    let height = $('#number1').val();
+    let weight = $('#number2').val();
+
     if(gender != 'vboy' && gender != 'vgirl')
     {
       alert('請選擇性別');
+      return;
+    }
+    else if(height == '' || weight == '')
+    {
+      alert('請輸入身高或體重');
       return;
     }
   })
